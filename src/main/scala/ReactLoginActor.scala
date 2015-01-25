9ec9a70fe444e2a782b78519cd942814c72efa8f
Alberto Ramirez <alberto@aramirez.es>
2015-01-25 20:23:57 +0100

    Add login system using cookies

--- src/main/scala/ReactLoginActor.scala
+++ src/main/scala/ReactLoginActor.scala
@@ -15,23 +15,42 @@ class ReactLoginActor extends Actor with LoginService {
 
 trait LoginService extends HttpService {
 
+  val secure_prefix = "3240dmiv00%$xsmo!a8730"
+
   val userRepository = Seq(
     ("user_1", "123456"),
     ("user_2", "123456"),
     ("user_3", "123456")
   )
 
+  var loggedInUsers = Seq[(String, String)]()
+
   def checkLogin(user_name: String, user_pass: String): Boolean = {
     userRepository.exists(tuple => tuple._1 == user_name && tuple._2 == user_pass)
   }
 
-  val user = "Alberto"
+  def logUserIn(user_name: String, user_password: String): HttpCookie = {
+    val md = java.security.MessageDigest.getInstance("SHA-1");
+    val token = md.digest((secure_prefix.concat(user_name).concat(System.currentTimeMillis.toString)).getBytes).toString
+    loggedInUsers = loggedInUsers :+ (token, user_name)
+    println("new user logged in " + user_name + " with token " + token)
+    HttpCookie("logged", content = token)
+  }
+
   val loginRoute =
     get {
       respondWithMediaType(`text/html`) {
         path("") {
-          complete {
-            html.index.render().toString()
+          optionalCookie("logged") {
+            case Some(nameCookie) => complete {
+              loggedInUsers.find((tuple => tuple._1 == nameCookie.content)) match {
+                case Some(userName) => html.index.render(userName._2).toString()
+                case None => html.index.render("").toString()
+              }
+            }
+            case None => complete {
+              html.index.render("").toString()
+            }
           }
         } ~
         path("login") {
@@ -41,17 +60,17 @@ trait LoginService extends HttpService {
         } ~
         path("page1") {
           complete {
-            html.page.render(user).toString()
+            html.page.render().toString()
           }
         } ~
         path("page2") {
           complete {
-            html.page.render(user).toString()
+            html.page.render().toString()
           }
         } ~
         path("page3") {
           complete {
-            html.page.render(user).toString()
+            html.page.render().toString()
           }
         }
       }
@@ -59,8 +78,8 @@ trait LoginService extends HttpService {
     post {
       path("login") {
         formFields('user_name, 'user_password) { (user_name, user_password) =>
-          validate(checkLogin(user_name, user_password), "Email and password not valid") {
-            setCookie(HttpCookie("logged", user_name)) {
+          validate(checkLogin(user_name, user_password), "Email or password not valid") {
+            setCookie(logUserIn(user_name, user_password)) {
               // Redirect to "" does not work. Hardcoded until find a fix to that.
               redirect("http://localhost:8080", StatusCodes.MovedPermanently)
             }
--- src/test/scala/ReactLoginActorSpec.scala
+++ src/test/scala/ReactLoginActorSpec.scala
@@ -1,11 +1,12 @@
 package com.aramirez
 
 import org.specs2.mutable.Specification
+import spray.routing.ValidationRejection
 import spray.testkit.Specs2RouteTest
 import spray.http._
 import StatusCodes._
 
-class MyServiceSpec extends Specification with Specs2RouteTest with LoginService {
+class LoginServiceSpec extends Specification with Specs2RouteTest with LoginService {
   def actorRefFactory = system
 
   "LoginService" should {
@@ -20,6 +21,24 @@ class MyServiceSpec extends Specification with Specs2RouteTest with LoginService
       Get("/login") ~> loginRoute ~> check {
         status === OK
         responseAs[String] must contain("<form action=\"/login\" method=\"post\">")
+      }
+    }
+
+    "return Bad Request when POST to /login without mandatory fields" in {
+      Post("/login") ~> sealRoute(loginRoute) ~> check {
+        status === StatusCodes.BadRequest
+      }
+    }
+
+    "return ValidationRejection when POST to /login with invalid credentials" in {
+      Post("/login", FormData(Seq("user_name" -> "aramirez_", "user_password" -> "123456"))) ~> loginRoute ~> check {
+        rejection === ValidationRejection("Email or password not valid")
+      }
+    }
+
+    "return redirection and set Cookie when POST to /login with valid credentials" in {
+      Post("/login", FormData(Seq("user_name" -> "user_1", "user_password" -> "123456"))) ~> loginRoute ~> check {
+        status === StatusCodes.MovedPermanently
       }
     }
   }
