0ea13a4ef34a1383e08b700453fbd8bca9ddb590
Alberto Ramirez <alberto@aramirez.es>
2015-01-25 18:08:42 +0100

    Add base to log in

--- src/main/scala/ReactLoginActor.scala
+++ src/main/scala/ReactLoginActor.scala
@@ -10,9 +10,20 @@ class ReactLoginActor extends Actor with LoginService {
   def actorRefFactory = context
 
   def receive = runRoute(loginRoute)
+
 }
 
 trait LoginService extends HttpService {
+
+  val userRepository = Seq(
+    ("user_1", "123456"),
+    ("user_2", "123456"),
+    ("user_3", "123456")
+  )
+
+  def checkLogin(user_name: String, user_pass: String): Boolean = {
+    userRepository.exists(tuple => tuple._1 == user_name && tuple._2 == user_pass)
+  }
 
   val user = "Alberto"
   val loginRoute =
@@ -41,6 +52,17 @@ trait LoginService extends HttpService {
         path("page3") {
           complete {
             html.page.render(user).toString()
+          }
+        }
+      }
+    } ~
+    post {
+      path("login") {
+        formFields('user_name, 'user_password) { (user_name, user_password) =>
+          validate(checkLogin(user_name, user_password), "Email and password not valid") {
+            setCookie(HttpCookie("logged", user_name)) {
+              redirect("http://localhost:8080", StatusCodes.MovedPermanently)
+            }
           }
         }
       }
